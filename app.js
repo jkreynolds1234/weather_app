6720e1defff1235edc385739b92cf57bb0198ee1
Jenna Reynolds <reynolds.jennak@gmail.com>
2024-06-18 15:59:51 -0700

    Style updates

--- app.js
+++ app.js
@@ -25,35 +25,46 @@ app.post("/", function(req, res){
 
     // https get request to the api
     https.get(url, function(response){
-        console.log(response.statusCode);
+        // console.log(response.statusCode);
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
+            // console.log(weatherData);
             // temp response in Kelvins --> convert to fahrenheit
             temp = ((((weatherData.main.temp)-273.15)*9)/5)+32;
-            temp = +temp.toFixed(2)
+            temp = +temp.toFixed(2);
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             html = `
-                <head><meta charset="utf-8"></head>
-                <body>
-                    <h1>Current Weather in ${query}</h1>
-                    <div id="tempContainer">
-                        <img src="${imageURL}">
-                        <div>
-                            <h3>${temp}°F</h3>
-                            <h3>${weatherDescription}</h3>
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link rel="stylesheet" href="/css/bootstrap.min.css"/>
+                        <link href="/styles.css" rel="stylesheet">
+                    </head>
+                    <body>
+                        <h2>Weather in ${query}</h2>
+                        <div id="tempContainer">
+                            <img src="${imageURL}">
+                            <div id="tempDescription">
+                                <h3>${temp}°F</h3>
+                                <h3>${weatherDescription}</h3>
+                            </div>
                         </div>
-                    </div>
-                </body>
+                        <script language="javascript" src="/js/bootstrap.min.js"></script>
+                    </body>
+                </html>
             `;
-            res.write(html)
+            res.write(html);
             // res.write('<head><meta charset="utf-8"></head>');
             // res.write("<h1>The temperature in " + query + " is " + temp + " degrees Fahrenheit.</h1>");
             // res.write("<p>The weather is currently " + weatherDescription + "</p>");
             // res.write("<img src=" + imageURL +">");
-            console.log(url)
+            // console.log(url);
             res.send();
         });
     });
